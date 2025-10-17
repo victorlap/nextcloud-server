MarmoPL/server

feat(database): introduce Snowflake IDs generator
Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

lib/private/SnowflakeIdGenerator.php
@@ -1,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+namespace OC;
+
+/**
+ * Nextcloud Snowflake ID generator
+ *
+ * Generates unique ID for database
+ *
+ * @since 33.0.0
+ */
+final class SnowflakeIdGenerator {
+	public function __invoke(): int|float {
+		// Time related
+		$microtime = microtime(true);
+		$seconds = ((int)$microtime) - SnowflakeId::TS_OFFSET;
+		$milliseconds = ((int)($microtime * 1000)) % 1000;
+
+		$serverId = $this->getServerId() & 0x1FF; // Keep 9 bits
+		$isCli = (int)$this->isCli(); // 1 bit
+		$sequenceId = $this->getSequenceId($seconds, $milliseconds); //  12 bits
+		if ($sequenceId > 0xFFF) {
+			// Throttle a bit, wait for next millisecond
+			usleep(1000);
+			return $this();
+		}
+
+		$firstHalf = $seconds & 0x7FFFFFFF;
+		$secondHalf = (($milliseconds & 0x3FF) << 22) | ($serverId << 13) | ($isCli << 12) | $sequenceId;
+		if (PHP_INT_SIZE === 8) {
+			return $firstHalf << 32 | $secondHalf;
+		}
+
+		// Fallback for 32 bits systems
+		return hexdec(bin2hex(pack('NN', $firstHalf, $secondHalf)));
+	}
+
+	private function getServerId(): int {
+		return crc32(gethostname() ?: random_bytes(8));
+	}
+
+	private function isCli() {
+		return PHP_SAPI === 'cli';
+	}
+
+	private function getSequenceId(int $seconds, int $milliseconds): int {
+		if ($this->isCli()) {
+			// APCu cache isn’t shared between CLI processes
+			return random_int(0, 0xFFF - 1);
+		}
+
+		if (function_exists('apcu_inc')) {
+			$key = 'sequence:' . $seconds . ':' . $milliseconds;
+			$sequenceId = apcu_inc($key, ttl: 1);
+			if ($sequenceId === false) {
+				throw new \Exception('Failed to generate SnowflakeId with APCu');
+			}
+
+			return $sequenceId;
+		}
+
+		// TODO Implement file fallback?
+		throw new \Exception('Failed to get sequence Id');
+	}
+}

lib/public/ISnowflakeId.php
@@ -1,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+namespace OCP;
+
+/**
+ * Nextcloud ID generator
+ *
+ * Generates unique ID
+ * @since 33.0.0
+ */
+interface ISnowflakeId {
+	public const TS_OFFSET = 1759276800; // 2025-10-01 00:00:00
+
+	/**
+	 * Returns sequence ID as int (64 bits servers) or float (32 bits servers)
+	 *
+	 * This method is suitable to store Sequence Id in database.
+	 * Use BIGINT (8 bytes)
+	 *
+	 * @since 33.0
+	 */
+	public function numeric(): int|float;
+
+	/**
+	 * Returns whether the SequenceId was created in CLI or not (eg. FPM, Apache)
+	 *
+	 * @since 33.0
+	 */
+	public function isCli(): bool;
+
+	/**
+	 * Returns the number of seconds after self::TS_OFFSET
+	 *
+	 * Creation time of the sequence ID since self::TS_OFFSET
+	 *
+	 * @since 33.0
+	 */
+	public function seconds(): int;
+
+	/**
+	 * Returns the number of milliseconds of creation
+	 *
+	 * @since 33.0
+	 */
+	public function milliseconds(): int;
+
+	/**
+	 * Returns full millisecond creation timestamp
+	 *
+	 * @since 33.0
+	 */
+	public function createdAt(): float;
+
+	/**
+	 * Returns server ID (encoded on 9 bits)
+	 *
+	 * @return int<0, 511>
+	 * @since 33.0
+	 */
+	public function serverId(): int;
+
+	/**
+	 * Returns sequence ID (encoded on 12 bits)
+	 *
+	 * @return int<0, 4095>
+	 * @since 33.0
+	 */
+	public function sequenceId(): int;
+}

victorlap/nextcloud-server

lib/private/SnowflakeIdGenerator.php
@@ -1,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+namespace OC;
+
+/**
+ * Nextcloud Snowflake ID generator
+ *
+ * Generates unique ID for database
+ *
+ * @since 33.0.0
+ */
+final class SnowflakeIdGenerator {
+	public function __invoke(): int|float {
+		// Time related
+		$microtime = microtime(true);
+		$seconds = ((int)$microtime) - SnowflakeId::TS_OFFSET;
+		$milliseconds = ((int)($microtime * 1000)) % 1000;
+
+		$serverId = $this->getServerId() & 0x1FF; // Keep 9 bits
+		$isCli = (int)$this->isCli(); // 1 bit
+		$sequenceId = $this->getSequenceId($seconds, $milliseconds); //  12 bits
+		if ($sequenceId > 0xFFF) {
+			// Throttle a bit, wait for next millisecond
+			usleep(1000);
+			return $this();
+		}
+
+		$firstHalf = $seconds & 0x7FFFFFFF;
+		$secondHalf = (($milliseconds & 0x3FF) << 22) | ($serverId << 13) | ($isCli << 12) | $sequenceId;
+		if (PHP_INT_SIZE === 8) {
+			return $firstHalf << 32 | $secondHalf;
+		}
+
+		// Fallback for 32 bits systems
+		return hexdec(bin2hex(pack('LL', $firstHalf, $secondHalf)));
+	}
+
+	private function getServerId(): int {
+		return crc32(gethostname() ?: random_bytes(8));
+	}
+
+	private function isCli() {
+		return PHP_SAPI === 'cli';
+	}
+
+	private function getSequenceId(int $seconds, int $milliseconds): int {
+		if ($this->isCli()) {
+			// APCu cache isn’t shared between CLI processes
+			return random_int(0, 0xFFF - 1);
+		}
+
+		if (function_exists('apcu_inc')) {
+			$key = 'sequence:' . $seconds . ':' . $milliseconds;
+			$sequenceId = apcu_inc($key, ttl: 1);
+			if ($sequenceId === false) {
+				throw new \Exception('Failed to generate SnowflakeId with APCu');
+			}
+
+			return $sequenceId;
+		}
+
+		// TODO Implement file fallback?
+		throw new \Exception('Failed to get sequence Id');
+	}
+}

lib/public/ISnowflakeId.php
@@ -1,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+namespace OCP;
+
+/**
+ * Nextcloud ID generator
+ *
+ * Generates unique ID
+ * @since 33.0.0
+ */
+interface ISnowflakeId {
+	public const TS_OFFSET = 1759276800; // 2025-10-01 00:00:00
+
+	/**
+	 * Returns sequence ID as int (64 bits servers) or string (32 bits servers)
+	 *
+	 * This method is suitable to store Sequence Id in database.
+	 * Use BIGINT (8 bytes)
+	 *
+	 * @since 33.0
+	 */
+	public function numeric(): int|string;
+
+	/**
+	 * Returns whether the SequenceId was created in CLI or not (eg. FPM, Apache)
+	 *
+	 * @since 33.0
+	 */
+	public function isCli(): bool;
+
+	/**
+	 * Returns the number of seconds after self::TS_OFFSET
+	 *
+	 * Creation time of the sequence ID since self::TS_OFFSET
+	 *
+	 * @since 33.0
+	 */
+	public function seconds(): int;
+
+	/**
+	 * Returns the number of milliseconds of creation
+	 *
+	 * @since 33.0
+	 */
+	public function milliseconds(): int;
+
+	/**
+	 * Returns full millisecond creation timestamp
+	 *
+	 * @since 33.0
+	 */
+	public function createdAt(): float;
+
+	/**
+	 * Returns server ID (encoded on 9 bits)
+	 *
+	 * @return int<0, 511>
+	 * @since 33.0
+	 */
+	public function serverId(): int;
+
+	/**
+	 * Returns sequence ID (encoded on 12 bits)
+	 *
+	 * @return int<0, 4095>
+	 * @since 33.0
+	 */
+	public function sequenceId(): int;
+}